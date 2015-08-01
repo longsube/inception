77457567e2286fcc43e0fa0f141bb5d0b601f2b9
Le Quang Long <longsube@gmail.com>
2015-08-02 04:33:32 +0700

    Update install_libvirt.sh

--- bin/install_libvirt.sh
+++ bin/install_libvirt.sh
@@ -6,10 +6,7 @@
 ## cannot be added into VM xml configuration).
 
 # Install KVM, QEMU and libvirt (0.9.8, so that we have /etc/init/libvirt-bin.conf)
-sudo apt-get -y install \
-    kvm \
-    qemu \
-    libvirt-bin
+sudo apt-get -y install libvirt-bin
 
 # Install dependencies. You can omit the libcurl4-gnutls-dev package
 # if you don’t want ESX support.
@@ -34,16 +31,14 @@ sudo chown root:root /opt/libvirt
 
 sudo chmod a+w /opt/libvirt
 cd /opt/libvirt
-wget http://libvirt.org/sources/libvirt-1.2.0.tar.gz
-tar xzvf libvirt-1.2.0.tar.gz
-mv libvirt-1.2.0 libvirt
+wget https://launchpad.net/ubuntu/+archive/primary/+files/libvirt_1.2.16.orig.tar.gz
+tar xzvf libvirt_1.2.16.orig.tar.gz
+mv libvirt-1.2.16 libvirt
 cd libvirt
 ./configure \
     --prefix=/usr \
     --localstatedir=/var \
     --sysconfdir=/etc \
-    --with-esx=yes \
-    --with-xen=yes # if you need Xen support, you’ll need to add
-		   # --with-xen=yes to the command
+    
 make -j
 sudo make install
